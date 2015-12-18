99dbeb61ee7b132fa5985e7c6968b895f62d32a3
Raj Abishek <rajabishek@hotmail.com>
2015-12-18 11:31:20 +0530

    Add the table name that the eloquent model points to

--- app/Task.php
+++ app/Task.php
@@ -6,4 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Task extends Model
 {
+    /**
+     * The database table used by the model.
+     *
+     * @var string
+     */
+    protected $table = 'tasks';
 }
