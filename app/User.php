4329554407b11bc4d3f8641a27f6c1d468fb1c55
Raj Abishek <rajabishek@hotmail.com>
2015-12-18 11:01:12 +0530

    Implement the contracts for password resets and pull in the traits

--- app/User.php
+++ app/User.php
@@ -2,9 +2,15 @@
 
 namespace Begin;
 
+use Illuminate\Auth\Authenticatable;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Auth\Passwords\CanResetPassword;
+use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
+use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
 
-class User extends Model
+class User extends Model implements AuthenticatableContract,
+                                    CanResetPasswordContract
 {
-    
+    use Authenticatable, CanResetPassword;
+
 }
