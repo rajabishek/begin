66c3004d82233aecc839a81e0ff647725a80fb1c
Raj Abishek <rajabishek@hotmail.com>
2015-12-20 18:00:39 +0530

    Add the vendor publish commands to the list of artisan commands

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Console;
+namespace Begin\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Laravel\Lumen\Console\Kernel as ConsoleKernel;
@@ -13,7 +13,7 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        //
+        'BasicIT\LumenVendorPublish\VendorPublishCommand',
     ];
 
     /**
