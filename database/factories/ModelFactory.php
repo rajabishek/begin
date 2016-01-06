34646b44a523ab186c178e423d671706b8746066
Raj Abishek <rajabishek@hotmail.com>
2016-01-06 10:52:11 +0530

    Adds the begin namespace reference in the factory

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -11,7 +11,7 @@
 |
 */
 
-$factory->define(App\User::class, function ($faker) {
+$factory->define(Begin\User::class, function ($faker) {
     return [
         'name' => $faker->name,
         'email' => $faker->email,
