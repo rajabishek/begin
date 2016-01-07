0aa934b6f9635aa4d38a229a55eb2da37b5cfea2
Raj Abishek <rajabishek@hotmail.com>
2016-01-07 19:08:40 +0530

    Register the service provider from the jwt-auth package

--- begin/gulpfile.js
+++ begin/gulpfile.js
@@ -15,18 +15,23 @@ require('laravel-elixir-vueify');
 
 var paths = {
 	'bootstrap': './vendor/bower_components/bootstrap/',
-    'bootswatch': './vendor/bower_components/bootstrap-theme-bootswatch-flatly/'
+    'bootswatch': './vendor/bower_components/bootstrap-theme-bootswatch-flatly/',
+    'jquery': './vendor/bower_components/jquery/',
+    'ladda': './node_modules/ladda/',
 }
 
 elixir(function(mix) {
 	
 	mix.styles([
         paths.bootswatch + 'css/bootstrap.min.css',
+        paths.ladda + 'dist/ladda.min.css',
     ], 'public/css/app.css', './');
 
     mix.copy(paths.bootstrap + 'fonts/**', 'public/fonts');
 
     mix.version('public/css/app.css');
-
+    
+    mix.scripts([paths.jquery + 'dist/jquery.min.js', paths.bootstrap + 'dist/js/bootstrap.min.js']);
+    
     mix.browserify('bootstrap.js', 'public/js/app.js');
 });
