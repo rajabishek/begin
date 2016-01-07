7f5c37d65579002307649b717b52d8574fa27980
Raj Abishek <rajabishek@hotmail.com>
2016-01-08 01:49:36 +0530

    Add the sample class for the base event listener

--- app/Listeners/Listener.php
+++ app/Listeners/Listener.php
@@ -1,11 +1,31 @@
 <?php
 
-namespace App\Listeners;
+namespace Begin\Listeners;
 
+use Begin\Events\PodcastWasPurchased;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
 
-abstract class Listener
+class EmailPurchaseConfirmation
 {
-    //
+    /**
+     * Create the event listener.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     *
+     * @param  PodcastWasPurchased  $event
+     * @return void
+     */
+    public function handle(PodcastWasPurchased $event)
+    {
+        // Access the podcast using $event->podcast...
+    }
 }
