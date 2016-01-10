534ee2335beebaca0d4e6af1838a9865176d2c30
Raj Abishek <rajabishek@hotmail.com>
2016-01-10 16:49:32 +0530

    Pulls in the newly changed namespace

--- begin/app/Http/Controllers/Auth/PasswordController.php
+++ begin/app/Http/Controllers/Auth/PasswordController.php
@@ -1,8 +1,8 @@
 <?php
 
-namespace App\Http\Controllers\Auth;
+namespace Begin\Http\Controllers\Auth;
 
-use App\Http\Controllers\Controller;
+use Begin\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\ResetsPasswords;
 
 class PasswordController extends Controller
