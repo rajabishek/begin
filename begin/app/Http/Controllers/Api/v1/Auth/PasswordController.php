a909fa9a7d23358302eb2dda55c55d68200d240d
Raj Abishek <rajabishek@hotmail.com>
2016-01-10 17:03:41 +0530

    Adds the controller to manage the password resets

---
+++ begin/app/Http/Controllers/Api/v1/Auth/PasswordController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Begin\Http\Controllers\Api\v1\Auth;
+
+use Begin\Http\Controllers\ApiController;
+use Illuminate\Foundation\Auth\ResetsPasswords;
+
+class PasswordController extends ApiController
+{
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Controller
+    |--------------------------------------------------------------------------
+    |
+    | This controller is responsible for handling password reset requests
+    | and uses a simple trait to include this behavior. You're free to
+    | explore this trait and override any methods you wish to tweak.
+    |
+    */
+
+    use ResetsPasswords;
+
+    /**
+     * Create a new password controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('guest');
+    }
+}
