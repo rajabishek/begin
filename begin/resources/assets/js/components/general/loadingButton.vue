91b97c1c3da31e88f8d83b4622b9e1a459843983
Raj Abishek <rajabishek@hotmail.com>
2016-01-11 00:19:13 +0530

    Adds the component that hs loading feature with the button

---
+++ begin/resources/assets/js/components/general/loadingButton.vue
@@ -0,0 +1,22 @@
+<template>
+	<button class="btn btn-{{size}} btn-{{type}}" type="submit"><i :class="{'fa': loading, 'fa-spinner': loading, 'fa-spin': loading}"></i> <slot></slot></button>
+</template>
+
+<script>
+module.exports = {
+    props: {
+    	loading: {
+            type: Boolean,
+            required: true
+        },
+        size: {
+            type: String,
+            required: true
+        },
+        type: {
+            type: String,
+            required: true
+        }
+    }
+};
+</script>
