708d1c7557859e131ad9200c280f3a8b2ddebd37
Raj Abishek <rajabishek@hotmail.com>
2016-01-10 16:46:45 +0530

    Pull in the spinkit css file for the loaders

--- begin/gulpfile.js
+++ begin/gulpfile.js
@@ -17,19 +17,19 @@ var paths = {
 	'bootstrap': './vendor/bower_components/bootstrap/',
     'bootswatch': './vendor/bower_components/bootstrap-theme-bootswatch-flatly/',
     'jquery': './vendor/bower_components/jquery/',
-    'ladda': './node_modules/ladda/',
+    'spinkit': './node_modules/spinkit/',
 }
 
 elixir(function(mix) {
 	
+    mix.sass('app.scss');
+
 	mix.styles([
         paths.bootswatch + 'css/bootstrap.min.css',
-        paths.ladda + 'dist/ladda.min.css',
+        paths.spinkit + 'css/spinkit.css',
     ], 'public/css/app.css', './');
 
     mix.copy(paths.bootstrap + 'fonts/**', 'public/fonts');
-
-    mix.version('public/css/app.css');
     
     mix.scripts([paths.jquery + 'dist/jquery.min.js', paths.bootstrap + 'dist/js/bootstrap.min.js']);
     
