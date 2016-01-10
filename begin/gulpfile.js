b8accdbe2fdd9060a3bfd8b65ac75485e42c06d7
Raj Abishek <rajabishek@hotmail.com>
2016-01-10 17:28:47 +0530

    Pull in all the frontend dependencies through npm instead of bower, do it in the vue way

--- begin/gulpfile.js
+++ begin/gulpfile.js
@@ -14,9 +14,9 @@ var elixir = require('laravel-elixir');
 require('laravel-elixir-vueify');
 
 var paths = {
-	'bootstrap': './vendor/bower_components/bootstrap/',
-    'bootswatch': './vendor/bower_components/bootstrap-theme-bootswatch-flatly/',
-    'jquery': './vendor/bower_components/jquery/',
+	'bootstrap': './node_modules/bootstrap/',
+    'bootswatch': './node_modules/bootswatch/',
+    'jquery': './node_modules/jquery/',
     'spinkit': './node_modules/spinkit/',
 }
 
@@ -25,11 +25,11 @@ elixir(function(mix) {
     mix.sass('app.scss');
 
 	mix.styles([
-        paths.bootswatch + 'css/bootstrap.min.css',
+        paths.bootswatch + 'flatly/bootstrap.min.css',
         paths.spinkit + 'css/spinkit.css',
     ], 'public/css/app.css', './');
 
-    mix.copy(paths.bootstrap + 'fonts/**', 'public/fonts');
+    mix.copy(paths.bootswatch + 'fonts/**', 'public/fonts');
     
     mix.scripts([paths.jquery + 'dist/jquery.min.js', paths.bootstrap + 'dist/js/bootstrap.min.js']);
     
