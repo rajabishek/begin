0112ee5e3f45c2de8c30ae0a0038b7d5f928124a
Raj Abishek <rajabishek@hotmail.com>
2016-01-11 00:30:01 +0530

    Adds the component that is responsible for logging out the user

---
+++ begin/resources/assets/js/components/views/auth/logout.vue
@@ -0,0 +1,15 @@
+<script>
+module.exports = {
+
+    route: {
+        activate: function(transition) {
+            this.$root.authenticated = false;
+            this.$root.user = null;
+            this.$root.token = null;
+            localStorage.removeItem('jwt-token');
+            transition.redirect('/');
+        }
+    }
+}
+</script>
+
