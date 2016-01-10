0c6e2924f9cc1ece880e76f53332dfbbc8ff7deb
Raj Abishek <rajabishek@hotmail.com>
2016-01-11 00:27:33 +0530

    Adds the base component that is responsible for displaying the tasks under tasks section

---
+++ begin/resources/assets/js/components/views/tasks.vue
@@ -0,0 +1,12 @@
+<template>
+	<nav-component></nav-component>
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-12"><router-view></router-view></div>
+		</div>
+	</div>
+	<footer-component></footer-component>
+</template>
+
+
+
