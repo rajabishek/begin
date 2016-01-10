b04a3796b815ba895ef6030244c0e53f4dd5baff
Raj Abishek <rajabishek@hotmail.com>
2016-01-10 16:48:58 +0530

    Change the namespace to begin

--- begin/app/Http/Controllers/Auth/AuthController.php
+++ begin/app/Http/Controllers/Auth/AuthController.php
@@ -1,10 +1,10 @@
 <?php
 
-namespace App\Http\Controllers\Auth;
+namespace Begin\Http\Controllers\Auth;
 
-use App\User;
+use Begin\User;
 use Validator;
-use App\Http\Controllers\Controller;
+use Begin\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\ThrottlesLogins;
 use Illuminate\Foundation\Auth\AuthenticatesAndRegistersUsers;
 
