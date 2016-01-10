052702c8f5c23a1dd9f58c14df9152bfc364c040
Raj Abishek <rajabishek@hotmail.com>
2016-01-10 16:55:08 +0530

    Change the namespace to reflect the new psr-4 standard followed

--- begin/config/services.php
+++ begin/config/services.php
@@ -30,7 +30,7 @@ return [
     ],
 
     'stripe' => [
-        'model'  => App\User::class,
+        'model'  => Begin\User::class,
         'key'    => env('STRIPE_KEY'),
         'secret' => env('STRIPE_SECRET'),
     ],
