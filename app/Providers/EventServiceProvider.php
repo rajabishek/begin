90b9bf5ef19eb8f1c168f6a974ca6bcf001b1c29
Raj Abishek <rajabishek@hotmail.com>
2016-01-15 12:10:57 +0530

    Chnages the directory structure of the providers

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace Begin\Providers;
 
+use Illuminate\Contracts\Events\Dispatcher as DispatcherContract;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
 
 class EventServiceProvider extends ServiceProvider
@@ -16,4 +17,30 @@ class EventServiceProvider extends ServiceProvider
             'Begin\Listeners\EventListener',
         ],
     ];
+
+    /**
+     * Register any other events for your application.
+     *
+     * @param  \Illuminate\Contracts\Events\Dispatcher  $events
+     * @return void
+     */
+    public function boot(DispatcherContract $events)
+    {
+        parent::boot($events);
+
+        $this->app['events']->listen('tymon.jwt.expired', function()
+        {
+            return response()->json(['success' => false,'errors' => ['Your token has expired']], 401);
+        });
+
+        $this->app['events']->listen('tymon.jwt.invalid', function()
+        {
+            return response()->json(['success' => false,'errors' => ['Your token is invalid']], 400);
+        });
+
+        $this->app['events']->listen('tymon.jwt.absent', function()
+        {
+            return response()->json(['success' => false,'errors' => ['Please provide a token']],400);
+        });
+    }
 }
